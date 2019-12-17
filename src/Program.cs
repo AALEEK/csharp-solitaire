3927c44d452d50502ac11b538b9c6083a57caeb5
Austin Leuenhagen <42649201+Lui798@users.noreply.github.com>
2019-12-17 14:52:11 -0600

    add cards and icon

--- src/Program.cs
+++ src/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace csharp_solitaire
+namespace csharp_solitaire.src
 {
     static class Program
     {
@@ -16,7 +16,7 @@ namespace csharp_solitaire
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Menu());
         }
     }
 }
