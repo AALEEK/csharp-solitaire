127bef59057758cf41735e70ee553af157341e55
Austin Leuenhagen <42649201+Lui798@users.noreply.github.com>
2019-12-19 15:28:36 -0600

    card class (#5)
    * add web files
    
    * starting fresh
    
    * add cards and icon
    
    * basic menu screen layout
    
    * all forms close now
    
    * why was this even here
    
    * card class started

--- src/form/Game.Designer.cs
+++ src/form/Game.Designer.cs
@@ -34,12 +34,13 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Game";
             this.Text = "Solitaire";
-            this.Load += new System.EventHandler(this.Game_Load);
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Game_FormClosing);
+            this.Load += new System.EventHandler(this.Game_Load);
             this.ResumeLayout(false);
 
         }
---
+++ src/game/Card.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csharp_solitaire.src.game
+{
+    class Card : PictureBox
+    {
+        public Card(string name)
+        {
+            BackColor = System.Drawing.Color.Transparent;
+            BackgroundImage = global::csharp_solitaire.Properties.Resources.red_back;
+            BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            Location = new System.Drawing.Point(12, 12);
+            Name = name;
+            Size = new System.Drawing.Size(80, 128);
+            TabIndex = 0;
+            TabStop = false;
+        }
+    }
+}
