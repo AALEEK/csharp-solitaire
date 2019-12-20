34def230c86b5f00da33d0cc88fb99e4d7984972
Austin Leuenhagen <42649201+Lui798@users.noreply.github.com>
2019-12-20 12:51:04 -0600

    ok (#7)
    * add web files
    
    * starting fresh
    
    * add cards and icon
    
    * basic menu screen layout
    
    * all forms close now
    
    * why was this even here
    
    * card class started
    
    * more work on card class

---
+++ src/game/CardColor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_solitaire.src.game
+{
+    class CardColor
+    {
+        private CardColor(string value) { Value = value; }
+
+        public string Value { get; set; }
+
+        public static CardColor BLACK { get { return new CardColor("BLACK"); } }
+        public static CardColor RED { get { return new CardColor("RED"); } }
+    }
+}
---
+++ src/game/CardSuit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_solitaire.src.game
+{
+    class CardSuit
+    {
+        private CardSuit(string value) { Value = value; }
+
+        public string Value { get; set; }
+
+        public static CardSuit SPADE { get { return new CardSuit("SPADE"); } }
+        public static CardSuit HEART { get { return new CardSuit("HEART"); } }
+        public static CardSuit CLUB { get { return new CardSuit("CLUB"); } }
+        public static CardSuit DIAMOND { get { return new CardSuit("DIAMOND"); } }
+    }
+}
